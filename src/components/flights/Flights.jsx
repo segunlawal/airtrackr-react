831855895c35b69af6aef945777ef95a1654cf55
Segun <lawalsegun72@gmail.com>
2023-05-09 02:05:35 +0100

    fix: loading state visibility

--- src/components/flights/Flights.jsx
+++ src/components/flights/Flights.jsx
@@ -78,7 +78,6 @@ const Flights = () => {
             );
         });
 
-    if (isLoading) return <p className="sm:pl-52 text-[#1e1e1e]">Loading...</p>;
     return (
         <div className="sm:pl-60 pl-2 py-5 pb-24 text-[#1e1e1e]">
             <Header />
@@ -86,7 +85,9 @@ const Flights = () => {
                 setFlights={setFlights}
                 setDisplayInfo={setDisplayInfo}
             />
-            <FlightsInfo flights={flights} displayInfo={displayInfo} />
+            {!isLoading && (
+                <FlightsInfo flights={flights} displayInfo={displayInfo} />
+            )}
 
             <div className="bg-white sticky top-0 text-sm text-custom-blue font-bold px-5 py-2 border-b-4 grid grid-cols-4 gap-6 md:gap-16 lg:gap-36 xl:gap-56">
                 <p>FLIGHT</p>
@@ -97,27 +98,31 @@ const Flights = () => {
                 <p>DEPARTURE DATE</p>
             </div>
 
-            <div className="">
-                {displayFlights}
-                <ReactPaginate
-                    previousLabel="<"
-                    nextLabel=">"
-                    pageCount={pageCount}
-                    onPageChange={handlePageChange}
-                    containerClassName="mt-10 flex gap-x-2 items-center"
-                    previousLinkClassName="rounded-md border-custom-blue border-[1px] text-2xl px-2"
-                    nextLinkClassName="rounded-md border-custom-blue border-[1px] text-2xl px-2"
-                    pageLinkClassName="text-lg px-2"
-                    disabledClassName=""
-                    activeClassName="border-[3px]"
-                    pageClassName=" rounded-sm border-[1px] border-custom-blue"
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={2}
-                />
-                {flights?.length === 0 && (
-                    <p className="text-center">No flights available</p>
-                )}
-            </div>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <div className="">
+                    {displayFlights}
+                    <ReactPaginate
+                        previousLabel="<"
+                        nextLabel=">"
+                        pageCount={pageCount}
+                        onPageChange={handlePageChange}
+                        containerClassName="mt-10 flex gap-x-2 items-center"
+                        previousLinkClassName="rounded-md border-custom-blue border-[1px] text-2xl px-2"
+                        nextLinkClassName="rounded-md border-custom-blue border-[1px] text-2xl px-2"
+                        pageLinkClassName="text-lg px-2"
+                        disabledClassName=""
+                        activeClassName="border-[3px]"
+                        pageClassName=" rounded-sm border-[1px] border-custom-blue"
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={2}
+                    />
+                    {flights?.length === 0 && (
+                        <p className="text-center">No flights available</p>
+                    )}
+                </div>
+            )}
         </div>
     );
 };
